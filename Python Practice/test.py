70e6b99e5e20ccbfab2fd7ee4a421f10abd106a2
kevinc03 <1cuikev@hdsb.ca>
2021-02-12 16:12:19 +0000

    exercise

--- Python Practice/test.py
+++ Python Practice/test.py
@@ -1,4 +1,9 @@
-language="python3"
-run="python 'Python Practice/Operator Practice/test.py'"
+# Practice Name: Feb 12 Practice
+# 
+# TODO: Python syntax
+# 
+# date and time completed: 2021-02-12
 
-print("Hello!")
+print("1460 Devon Rd.".center(70))
+print("Oakville, ON L6J 3L6.".center(70))
+
